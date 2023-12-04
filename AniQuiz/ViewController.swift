b382400bd58a9577d1200b69ab767c74559821a5
Leonardo Macedo <leonardomacedo@MacBook-Air-de-Leonardo.local>
2023-12-04 15:35:03 -0300

    first commit

---
+++ AniQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AniQuiz
+//
+//  Created by Leonardo Macedo on 04/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
