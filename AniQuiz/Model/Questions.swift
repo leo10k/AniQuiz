d46e8648a0bf26e5db87e4533e7acb224d1fb9d8
Leonardo Macedo <leonardomacedo@MacBook-Air-de-Leonardo.local>
2023-12-04 16:49:40 -0300

    creating the firt screen and navegation

---
+++ AniQuiz/Model/Questions.swift
@@ -0,0 +1,10 @@
+//
+//  Questions.swift
+//  AniQuiz
+//
+//  Created by Leonardo Macedo on 04/12/23.
+//
+
+import Foundation
+
+
---
+++ AniQuiz/ViewController/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  AniQuiz
+//
+//  Created by Leonardo Macedo on 04/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var startQuizButton: UIButton!
+    
+    @IBAction func pressedButton(_ sender: Any) {
+        print("The button has been pressed!")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configLayout()
+    }
+    
+    func configLayout() {
+        startQuizButton.layer.cornerRadius = 12.0
+        navigationItem.hidesBackButton = true
+    }
+
+}
+
